34baf5c6280d61406d5e2f698ba358bab00dd67e
maxidragon <maksymilian.gala1@gmail.com>
2023-12-11 10:46:47 +0100

    Day 9 part 1

---
+++ day9/part1/day9.py
@@ -0,0 +1,24 @@
+def extrapolate(history):
+    sequences = [list(map(int, history.split()))]
+    
+    while any(sequences[-1]):
+        differences = [sequences[-1][i + 1] - sequences[-1][i] for i in range(len(sequences[-1]) - 1)]
+        sequences.append(differences)
+
+    result = sequences[-1][-1]
+
+    for i in range(len(sequences) - 2, -1, -1):
+        result = sequences[i][-1] + result
+
+    return result
+
+
+file_path = "input.txt"
+with open(file_path, "r") as file:
+    oasis_lines = file.read().splitlines()
+
+    extrapolated_values = [extrapolate(line) for line in oasis_lines]
+    total_sum = sum(extrapolated_values)
+    print(total_sum)
+
+
